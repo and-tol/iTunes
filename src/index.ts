73d2652dd791b9ac566b2c7430f857ac3f28e79c
and-tol <tolstoi.a@gmail.com>
2020-07-30 19:06:12 +0300

    feat: create tabs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 // import './styles/style.min.css'
-
-// Variable
-const 
-
 import { audioPlayerInit } from './js/audioPlayer'
 import { radioPlayerInit } from './js/radioPlayer'
 import { videoPlayerInit } from './js/videoPlayer'
 
+// Variable
+const playerBtn: NodeListOf<Element> = document.querySelectorAll('.player-btn')
+const playerBlock: NodeListOf<Element> = document.querySelectorAll('.player-block')
+const temp: HTMLDivElement | null = document.querySelector('.temp')
 
+const deactivationPlayer = () => {
+  temp!.style.display = 'none'
+  playerBtn.forEach((element) => element.classList.remove('active'));
+  playerBlock.forEach((element) => element.classList.remove('active'));
+}
+
+
+// Events
+playerBtn.forEach((btn: Element, i: number) => {
+  btn.addEventListener('click', () => {
+    deactivationPlayer()
+    btn.classList.add('active')
+    playerBlock[i].classList.add('active')
+  })
+})
 
 // audioPlayerInit()
